ecc49c4be6ad77540e52df23a0d88469bb7806c3
Joel Jacob John <achuthengone03@gmail.com>
2023-07-27 17:45:12 +0530

    Update String matching last char.cpp

--- prepping/String matching last char.cpp
+++ prepping/String matching last char.cpp
@@ -40,90 +40,147 @@ using namespace std;
 
 //     return result;
 // }
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <unordered_map>
+
+// using namespace std;
+
+// struct Node {
+//     char value;
+//     Node* next;
+//     Node(char val) : value(val), next(nullptr) {}
+// };
+
+// struct Graph {
+//     Node* first;
+//     Node* last;
+//     Graph() : first(nullptr), last(nullptr) {}
+// };
+
+// Node* createNode(char value) {
+//     return new Node(value);
+// }
+
+// void addEdge(Graph& graph, char start, char end) {
+//     Node* startNode = createNode(start);
+//     Node* endNode = createNode(end);
+
+//     if (graph.first == nullptr) {
+//         graph.first = startNode;
+//         graph.last = endNode;
+//     } else {
+//         graph.last->next = startNode;
+//         graph.last = endNode;
+//     }
+// }
+
+// bool dfsWithMemoization(Node* currentNode, char target, vector<bool>& visited, vector<vector<bool>>& memo) {
+//     if (currentNode->value == target) {
+//         return true;
+//     }
+
+//     if (visited[currentNode->value - 'a']) {
+//         return false;
+//     }
+
+//     visited[currentNode->value - 'a'] = true;
+
+//     Node* neighbor = currentNode->next;
+//     while (neighbor != nullptr) {
+//         if (dfsWithMemoization(neighbor, target, visited, memo)) {
+//             memo[currentNode->value - 'a'][target - 'a'] = true;
+//             memo[target - 'a'][currentNode->value - 'a'] = true; // Update the opposite direction in the memo array
+//             return true;
+//         }
+//         neighbor = neighbor->next;
+//     }
+
+//     return false;
+// }
+
+// string solution(vector<string> A, int N) {
+//     string result;
+//     Graph graph;
+
+//     // Create the graph representation of the strings
+//     for (int i = 0; i < N; i++) {
+//         addEdge(graph, A[i][0], A[i][1]);
+//     }
+
+//     // Initialize memoization array
+//     vector<vector<bool>> memo(26, vector<bool>(26, false));
+
+//     // Check for connectivity for each K
+//     for (int K = 0; K < N; K++) {
+//         result += '0';
+
+//         // Perform DFS with memoization
+//         vector<bool> visited(26, false);
+//         for (int i = 0; i < 26; i++) {
+//             if (dfsWithMemoization(graph.first, A[K][1], visited, memo)) {
+//                 result[K] = '1';
+//                 break; // If the current string chain can be connected, move to the next K.
+//             }
+//             visited.assign(26, false); // Reset visited array for each DFS call
+//         }
+//     }
+
+//     return result;
+// }
+// char first(string s){
+//     return s[0];
+// }
+// char last(string s){
+//     return s[1];
+// }
+// string soln(vector<string>A,int N){
+//     string s="1";
+//     for(int i=1;i<N;i++){
+//         s+='0';
+//     }
+//     for(int i=0;i<N;i++){
+//         for(int j=0;j<i;j++){
+//             if(last(A[i])==first(A[j])){
+//                 s[i]='1';
+//             }
+
+//         }
+//     }
+//     return s;
+// }
+
+// int main() {
+//     vector<string> A = {"he", "ll", "lo","el"};
+//     int N = A.size();
+//     string result = soln(A, N);
+//     cout << result << endl; // Output: "1001"
+
+//     return 0;
+// }
 #include <iostream>
 #include <vector>
-#include <string>
-#include <unordered_map>
-
 using namespace std;
 
-struct Node {
-    char value;
-    Node* next;
-    Node(char val) : value(val), next(nullptr) {}
-};
-
-struct Graph {
-    Node* first;
-    Node* last;
-    Graph() : first(nullptr), last(nullptr) {}
-};
-
-Node* createNode(char value) {
-    return new Node(value);
+bool canConnect(const string& s1, const string& s2) {
+    return s1[1] == s2[0];
 }
 
-void addEdge(Graph& graph, char start, char end) {
-    Node* startNode = createNode(start);
-    Node* endNode = createNode(end);
+string solution(vector<string>& A) {
+    int N = A.size();
+    string result(N, '0');  // Initialize result string with '0'
 
-    if (graph.first == nullptr) {
-        graph.first = startNode;
-        graph.last = endNode;
-    } else {
-        graph.last->next = startNode;
-        graph.last = endNode;
-    }
-}
+    // Base case: First string can always be connected
+    result[0] = '1';
 
-bool dfsWithMemoization(Node* currentNode, char target, vector<bool>& visited, vector<vector<bool>>& memo) {
-    if (currentNode->value == target) {
-        return true;
-    }
-
-    if (visited[currentNode->value - 'a']) {
-        return false;
-    }
-
-    visited[currentNode->value - 'a'] = true;
-
-    Node* neighbor = currentNode->next;
-    while (neighbor != nullptr) {
-        if (dfsWithMemoization(neighbor, target, visited, memo)) {
-            memo[currentNode->value - 'a'][target - 'a'] = true;
-            memo[target - 'a'][currentNode->value - 'a'] = true; // Update the opposite direction in the memo array
-            return true;
-        }
-        neighbor = neighbor->next;
-    }
-
-    return false;
-}
-
-string solution(vector<string> A, int N) {
-    string result;
-    Graph graph;
-
-    // Create the graph representation of the strings
-    for (int i = 0; i < N; i++) {
-        addEdge(graph, A[i][0], A[i][1]);
-    }
-
-    // Initialize memoization array
-    vector<vector<bool>> memo(26, vector<bool>(26, false));
-
-    // Check for connectivity for each K
-    for (int K = 0; K < N; K++) {
-        result += '0';
-
-        // Perform DFS with memoization
-        vector<bool> visited(26, false);
-        for (int i = 0; i < 26; i++) {
-            if (dfsWithMemoization(graph.first, A[K][1], visited, memo)) {
-                result[K] = '1';
-                break; // If the current string chain can be connected, move to the next K.
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (canConnect(A[j], A[i]) && result[j] == '1') {
+                // If A[j] can be connected and A[0] to A[j] can be connected, then A[0] to A[i] can also be connected
+                result[i] = '1';
+                break;
             }
-            visited.assign(26, false); // Reset visited array for each DFS call
         }
     }
 
@@ -132,9 +189,9 @@ string solution(vector<string> A, int N) {
 
 int main() {
     vector<string> A = {"he", "ll", "lo", "el"};
-    int N = A.size();
-    string result = solution(A, N);
-    cout << result << endl; // Output: "1001"
+    string result = solution(A);
+
+    cout << result << endl;  // Output: "1110"
 
     return 0;
 }
